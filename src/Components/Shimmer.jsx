6e014c217b1beb81951c454e6065a40ae4520fff
Yash <yaswanth.chundru@gmail.com>
2025-03-28 11:15:21 +0530

    shimmer commit

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -3,36 +3,24 @@ const Shimmer = () => {
   return (
     <div className="w-screen h-screen">
         <div className="m-3 p-2 flex gap-2 bg-black/80 w-screen h-70">
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
         </div>
         <div className="m-3 p-2 flex gap-2 bg-black/80 w-screen h-70">
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
         </div>
         <div className="m-3 p-2 flex gap-2 bg-black/80 w-screen h-70">
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
         </div>
         <div className="m-3 p-2 flex gap-2 bg-black/80 w-screen h-70">
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
-          <div className="border border-gray-600 w-2/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
+          <div className="border border-gray-600 w-4/12 p-2"></div>
         </div>
         
     </div>
