3d91a616ab6efd9dfff30de733c9ae072aa1ae0a
Yash <yaswanth.chundru@gmail.com>
2025-03-28 10:54:40 +0530

    new list of movies

--- src/Components/AllMovies.jsx
+++ src/Components/AllMovies.jsx
@@ -6,13 +6,16 @@ const AllMovies = () => {
     // if(nowPlayingMovies) return
     // console.log(allMovies.nowPlaying)
   return (
-    <div className="text-white absolute z-50 mt-[33%] md:mt-[38%] w-[100%] pb-10 h-auto">
+    <div className="text-white absolute z-50 mt-[35%] md:mt-[38%] w-[100%] pb-10">
         <MovieList title={"Now Playing"} movies={allMovies.nowPlaying}/>
         <MovieList title={"Top Rated"} movies={allMovies.topRated}/>
         <MovieList title={"Popular"} movies={allMovies.popular}/>
         <MovieList title={"Upcoming"} movies={allMovies.upcoming}/>
-
-    </div>
+        <MovieList title={"In Theater's"} movies={allMovies.nowPlaying}/>
+        <MovieList title={"All Time Hits"} movies={allMovies.topRated}/>
+        <MovieList title={"Popular Movies"} movies={allMovies.popular}/>
+        <MovieList title={"Upcoming Movies"} movies={allMovies.upcoming}/>
+      </div>
   )
 }
 
